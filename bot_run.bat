dc9e09416c1e8adb1175d2571b96e1aefa1bbb65
dmitry zmeev <dzmeev59@gmail.com>
2022-09-14 11:44:23 +0500

    second commit

---
+++ bot_run.bat
@@ -0,0 +1,12 @@
+@echo off
+
+call %~dp0python_bot_pandas\venv\scripts\activate
+
+cd %~dp0python_bot_pandas
+
+
+set TOKEN=5697385909:AAE6HLFD0JcSUk1Nkk_YM7RNRhvCDuyqF3I
+
+python bot_panda.py
+
+pause
